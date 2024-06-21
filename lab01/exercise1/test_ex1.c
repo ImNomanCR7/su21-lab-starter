26dacdd3ec291a38004cdfe97416f577c413678e
Noman Rafiq <noman.rafiq@10xengineers.com>
2024-06-21 17:32:01 +0500

    Lab01 Solved

--- lab01/exercise1/test_ex1.c
+++ lab01/exercise1/test_ex1.c
@@ -13,6 +13,10 @@ int main(int argc, char **argv) {
 
     int num_z = num_occurrences(str, 'z');
     assert(num_z == 0);
+    
+    //Noman's Test Case
+    int num_o = num_occurrences(str, 'o');
+    assert(num_o == 2);
 
     /* TODO: Think of a scenario that is not tested by the current test cases. Create one additional test case to test this scenario. */
 
@@ -36,6 +40,15 @@ int main(int argc, char **argv) {
     assert(dna_seq_2.T_count == 0);
 
     /* TODO: Think of a scenario that is not tested by the current test cases. Create one additional test case to test this scenario. */
+    
+    //Noman's Test case 2
+    DNA_sequence dna_seq_3;
+    strcpy(dna_seq_3.sequence, "AACGTACAGTTT");
+    compute_nucleotide_occurrences(&dna_seq_3);
+    assert(dna_seq_3.A_count == 4);
+    assert(dna_seq_3.C_count == 2);
+    assert(dna_seq_3.G_count == 2);
+    assert(dna_seq_3.T_count == 4);
 
     printf("Congrats! If you have made it to this line, your Part 2 Test cases are all passing!\n");
 
